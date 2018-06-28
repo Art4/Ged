7bb0d988de195b8ede2d1805aae5c913c24a8a9d
Art4 <art4@wlabs.de>
2018-06-28 11:45:37 +0200

    Rotate menu icon on click

--- pages/search.js
+++ pages/search.js
@@ -50,9 +50,30 @@ inputField.addEventListener('keyup', (event) => {
 searchWin.addEventListener('mouseover', (event) => {
     Utils.changeWindowOpacity(1.0);
 });
+
 searchWin.addEventListener('mouseleave', (event) => {
     // wait 100ms to avoid racecondition with mouseover event
     setTimeout(function() {
         Utils.changeWindowOpacity(0.4);
     }, 100);
 });
+
+// Let the menu icon rotate
+(function () {
+    var button = document.getElementById('menu-button');
+    var rotated = true;
+
+    button.addEventListener('click', (event) => {
+        if (rotated === false) {
+            return;
+        }
+
+        rotated = false;
+
+        setTimeout(() => {
+            rotated = true;
+        }, 250);
+
+        document.getElementById('menu-button-icon').classList.toggle('open');
+    });
+})();
