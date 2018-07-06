352c88d5ece8b134d0437672d7311b132f51bbcf
Art4 <art4@wlabs.de>
2018-07-06 15:53:10 +0200

    Add Draft object

---
+++ tests/unit/draft-test.js
@@ -0,0 +1,57 @@
+/*
+ * Ged
+ * Copyright (C) 2011-2018  Artur Weigandt  https://wlabs.de/kontakt
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+const File = require('../../src/file.js');
+const Draft = require('../../src/draft.js');
+
+describe("The draft", function() {
+    describe('without data', () => {
+        it('returns empty array', () => {
+            var draft = new Draft('12345', []);
+
+            expect(draft.getFiles()).toEqual([]);
+            expect(draft.getNearestFile()).toBe(null);
+        });
+    });
+
+    describe('with file list', () => {
+        var files = [
+            new File('12345.dft', []),
+            new File('12345.pdf', []),
+        ];
+
+        it('returns file list', () => {
+            var draft = new Draft('12345', files);
+
+            expect(draft.getFiles()).toBe(files);
+            expect(draft.getNearestFile()).toBe(null);
+        });
+    });
+
+    describe('without file list and nearestFile', () => {
+        var files = [];
+        var nearestFile = new File('12344.dft');
+
+        it('returns empty array and nearestFile', () => {
+            var draft = new Draft('12345', files, nearestFile);
+
+            expect(draft.getFiles()).toBe(files);
+            expect(draft.getNearestFile()).toBe(nearestFile);
+        });
+    });
+});
