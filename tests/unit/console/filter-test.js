029626a42defdcb7d7ff23c259258e3442dd64bd
Art4 <art4@wlabs.de>
2019-01-24 10:09:57 +0100

    Create Filter

---
+++ tests/unit/console/filter-test.js
@@ -0,0 +1,29 @@
+/*
+ * Ged
+ * Copyright (C) 2011-2019  Artur Weigandt  https://wlabs.de/kontakt
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+const Filter = require('../../../src/console/filter.js');
+
+describe('The filter', function() {
+    describe('with comparison lt 5', () => {
+        var filter = new Filter('lt', 5);
+
+        it('returns true on test(4)', () => {
+            expect(filter.test(4)).toBe(true);
+        });
+    });
+});
